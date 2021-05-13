b597845300654c0e10d220fa15511b87e486822d
VIKI2017 <1206686486@qq.com>
2021-05-13 22:46:52 +0800

    改路径来啦

--- testcase/run.py
+++ testcase/run.py
@@ -10,8 +10,11 @@ if __name__ == '__main__':
     # pytest.main(['-s', '-q', '--alluredir', './result/', '--clean'])
     # pytest.main(['-v','--alluredir ./report'])
     # pytest.main(['-s','-q','--alluredir=../result'])
-    pytest.main(['-s','-q', '--alluredir', './result/xml', '-n=3'])
-    os.system("allure generate result/xml -o result/html --clean")
+    #/var/jenkins_home/workspace/TestSelenium/allure-report
+    #pytest.main(['-s','-q', '--alluredir', './result/xml', '-n=3'])
+    pytest.main(['-s', '-q', '--alluredir', '/var/jenkins_home/workspace/TestSelenium/allure-report/xml', '-n=3'])
+    os.system("allure generate /var/jenkins_home/workspace/TestSelenium/allure-report/xml -o /var/jenkins_home/workspace/TestSelenium/allure-report/html --clean")
+    #os.system("allure generate result/xml -o result/html --clean")
     # os.system(("allure serve allure"))
     #allure serve D:\LearnProject\SeleniumProject\testcase\result\xml
 
