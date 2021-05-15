df35f5a8648379d2d20db7c3cccadaac480eaec8
VIKI2017 <1206686486@qq.com>
2021-05-15 22:35:09 +0800

    改地址

--- testcase/run.py
+++ testcase/run.py
@@ -6,15 +6,15 @@ import os
 
 
 if __name__ == '__main__':
-    pytest.main()
+    #pytest.main()
 
     # pytest.main(['-s', '-q', '--alluredir', './result/', '--clean'])
     # pytest.main(['-v','--alluredir ./report'])
     # pytest.main(['-s','-q','--alluredir=../result'])
     #/var/jenkins_home/workspace/TestSelenium/allure-report
     #pytest.main(['-s','-q', '--alluredir', './result/xml', '-n=3'])
-    #pytest.main(['-s', '-q', '--alluredir', '/var/jenkins_home/workspace/TestSelenium/result/xml'])
-    #os.system("allure generate /var/jenkins_home/workspace/TestSelenium/result/xml -o /var/jenkins_home/workspace/TestSelenium/result/html --clean")
+    pytest.main(['-s', '-q', '--alluredir', '/var/jenkins_home/workspace/TestSelenium/result/xml'])
+    os.system("allure generate /var/jenkins_home/workspace/TestSelenium/result/xml -o /var/jenkins_home/workspace/TestSelenium/result/html --clean")
     #os.system("allure generate result/xml -o result/html --clean")
     # os.system(("allure serve allure"))
     #allure serve D:\LearnProject\SeleniumProject\testcase\result\xml
